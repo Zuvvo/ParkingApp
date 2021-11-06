fce17a3dc4555229ccfa0ae9c2fd5730876b899e
Zuvvo <Zuvvo4@gmail.com>
2021-11-06 13:34:46 +0100

    bootstrap fix

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -12,10 +12,6 @@ import 'bootstrap'
 require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
-require("channels")
-require("packs/custom")
-require("packs/posts.js")
-require("bootstrap")
 
 Rails.start()
 Turbolinks.start()
