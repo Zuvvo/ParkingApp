517e949556f9e26c622d910554cae4d2231326c6
Zuvvo <Zuvvo4@gmail.com>
2021-11-07 20:42:34 +0100

    slack wip test

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -2,3 +2,5 @@ Rails.application.config.middleware.use OmniAuth::Builder do
   provider :slack, '2707937853825.2695422973010', '3e145b07330ca7c4ad900b5863103f20',
            scope: 'team:read,users:read,identify,bot',  callback_path: '/auth/slack/callback'
 end
+
+OmniAuth.config.allowed_request_methods = %i[get]
