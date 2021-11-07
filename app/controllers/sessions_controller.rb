a088bcb6fb6a38bb22b1146d53b61472d7a37923
Zuvvo <Zuvvo4@gmail.com>
2021-11-07 19:21:24 +0100

    slack wip

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,6 @@
 class SessionsController < ApplicationController
   def create
-    @user = User.find_or_create_by(full_name: auth_hash.info.name, member_id: 3)
+    @user = User.find_or_create_by(full_name: auth_hash.info.name, member_id: auth_hash.info.user_id)
     session[:user_id] = @user.id
     redirect_to '/'
   end
