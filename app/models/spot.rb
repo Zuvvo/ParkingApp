92e253504e650babba9a9b2325c9afbba76991a1
Zuvvo <Zuvvo4@gmail.com>
2021-11-07 11:06:14 +0100

    slack gem added, app frontend wip

--- app/models/spot.rb
+++ app/models/spot.rb
@@ -1,3 +1,14 @@
 class Spot < ApplicationRecord
   validates :number, presence: true
+
+  def set(number)
+    puts number
+    self.number = number
+    self.book_time = Time.now.strftime("%Y-%d-%m %H:%M:%S %Z")
+  end
+
+  def test
+    puts 'X'
+  end
+
 end
