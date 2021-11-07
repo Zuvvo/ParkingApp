d334240736ae2509b3e489cda786557e3dbaf326
Zuvvo <Zuvvo4@gmail.com>
2021-11-07 18:57:05 +0100

    slack wip

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,10 +1,3 @@
 class WelcomeController < ApplicationController
-  def index
 
-  end
-
-  def current_user
-    return unless session[:user_id]
-    @current_user ||= User.find(session[:user_id])
-  end
 end
